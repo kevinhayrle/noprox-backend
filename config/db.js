aaa70e845dfd046b07d1af0205edeec3aab9b925
Kevin Antony <kevinantonyofficial@gmail.com>
2025-09-20 23:24:35 +0530

    v

--- config/db.js
+++ config/db.js
@@ -2,17 +2,28 @@ const mysql = require("mysql2");
 const fs = require("fs");
 const path = require("path");
 
+let sslConfig;
+try {
+  sslConfig = {
+    ca: fs.readFileSync('ca.pem')
+  };
+} catch (err) {
+  console.error('❌ Could not load ca.pem:', err.message);
+  process.exit(1); 
+}
+
 const db = mysql.createConnection({
   host: 'mysql-16673040-godwin123.c.aivencloud.com',
   user: 'avnadmin',
   password: process.env.DB_PASSWORD,
   database: "nope_db",
   port: 20134,
-  ssl: {
-    ca: fs.readFileSync(path.join(__dirname, 'aiven-ca.pem')),
-    rejectUnauthorized: true
-  }
+   ssl: sslConfig,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
 
 db.connect((err) => {
   if (err) {
